e5ec25b23bebfabdb3f963fab393af6000885424
Aniket Pandharabale <aniketicloud@gmail.com>
2022-01-30 00:09:21 +0530

    api calls

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,33 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, {
+  render,
+  Fragment,
+  Text,
+  IssuePanel,
+  useProductContext,
+  useState,
+} from "@forge/ui";
+import api, { route } from "@forge/api";
+
+const fetchCommentsForIssue = async (issueId) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueId}/comment`);
+
+  const data = await res.json();
+  return data.comments;
+};
 
 const App = () => {
+  const context = useProductContext();
+
+  const [comments] = useState(
+    async () => await fetchCommentsForIssue(context.platformContext.issueKey)
+  );
+  console.log(`Number of comments on this issue: ${comments.length}`);
+
   return (
     <Fragment>
-      <Text>Hello world!</Text>
+      <Text>Hello Aniket</Text>
     </Fragment>
   );
 };
